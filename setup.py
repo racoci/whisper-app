e62ba872ce07a623a26b14f76198db833911cecd
Lucas Racoci <racoci.0@gmail.com>
2024-04-06 15:09:16 -0300

    Update setup.py using [whisper](https://github.com/openai/whisper/blob/main/setup.py)'s as reference

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+import pkg_resources
+from pathlib import Path
 
 setup(
     name='whisper-app',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'whisper @ git+https://github.com/openai/whisper.git',
-        'librosa'
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            Path(__file__).with_name("requirements.txt").open()
+        )
     ],
     author='Lucas Racoci',
     author_email='racoci.0@gmail.com',
